ce15c3dd95ffde05af181dd9c873950cee4f9e75
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-05 06:25:23 +0530

    Update tasks.py

--- hanna/tasks.py
+++ hanna/tasks.py
@@ -1990,7 +1990,9 @@ def process_prompts4(final_content, language):
         #print("Full LLM Response:\n", generated_text)
         print(f"Raw AI Response: {generated_response}")
         cleaned_text = re.sub(r"```json|```", "", generated_response).strip()
-        json_response = json.loads(cleaned_text)
+        json_response = extract_json_from_response(cleaned_text)
+        #print(f"json_data: {json_data}")
+        #json_response = json.loads(cleaned_text)
         print(f"json_response: {json_response}")
            
 
