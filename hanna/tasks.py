ff18d1fe978890a07e779158e7572a5f9cd61d82
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-05 23:33:06 +0530

    Update tasks.py

--- hanna/tasks.py
+++ hanna/tasks.py
@@ -22,6 +22,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import ConfigurableField
 from langchain_openai import ChatOpenAI
 from together import Together
+import fasttext
 import logging
 
 @shared_task
@@ -40,6 +41,36 @@ chat_template = (
     "{% endif %}{% endfor %}"
 )
 
+def detectLanguage( text, original_language):
+    # Load the pre-trained language identification model
+    model_path = 'lid.176.ftz'  # Path to the pre-trained model file
+    model = fasttext.load_model(model_path)
+    # Text to be identified
+    text = text.replace('\n', ' ').strip()  # Remove newlines and trim whitespace
+    # If the text is less than 5 words, return the original language
+    if len(text.split()) < 5:
+        return original_language
+    # Predict the language of the text
+    predicted_languages = model.predict(text, k=1)  # Get top 1 prediction
+    detected_language_code = predicted_languages[0][0].replace('__label__', '')
+    # Mapping of language codes to language names. I think with these languages we have more than enough
+    language_names = {
+        'en': 'English',
+        'es': 'Spanish',
+        'fr': 'French',
+        'de': 'German',
+        'it': 'Italian',
+        'pt': 'Portuguese',
+        'zh': 'Chinese',
+        'ru': 'Russian',
+        'ja': 'Japanese',
+        'nl': 'Dutch',
+        'ar': 'Arabic',
+        'ur': 'Urdu'
+    }
+    # Return the full name of the detected language
+    return language_names.get(detected_language_code, original_language)
+    
 @shared_task
 def evaluate_text_task(note_text, guidelines):
     try:
