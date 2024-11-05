19ad80478c2877340d49611c3336ebf5a99398e7
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-05 10:54:39 +0530

    Update views.py

--- hanna/views.py
+++ hanna/views.py
@@ -1200,6 +1200,12 @@ def DA_tester(request):
         elif selected_option == 'option3':
             # Trigger process_prompts3 for option 3 (e.g., Data Analysis Option 3)
             task = process_prompts_3.apply_async(args=[user_input, language])
+        elif selected_option == 'option4':
+            # Trigger process_prompts3 for option 3 (Strategic Analysis for Cognitive Dynamics)
+            response_data = trigger_ppt_generation(final_content, language)
+            if 'error' in response_data:
+                return Response(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse(response_data, status=status.HTTP_202_ACCEPTED)
         else:
             return JsonResponse({'error': 'Invalid option selected'}, status=status.HTTP_400_BAD_REQUEST)
 
