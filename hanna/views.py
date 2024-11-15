26d0e976c97ec7a442400b8965535c618527f5c2
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-15 08:26:06 +0530

    Update views.py

--- hanna/views.py
+++ hanna/views.py
@@ -276,6 +276,14 @@ def webhook_handler(request):
             except json.JSONDecodeError as e:
                 return JsonResponse({"error": f"Failed to parse classification result: {str(e)}"}, status=500)
 
+            payload = {
+                "Company_ID": company_id,
+                "Initiative_ID": initiative_id,
+                "Date": date,  # Use the provided date from the payload
+                "areas": areas  # Parse classification JSON
+            }
+            print(payload)
+            result = insert_classification_data(payload)
             if result.get("status") == "success":
                 return JsonResponse({
                     "status": "success",
@@ -283,6 +291,9 @@ def webhook_handler(request):
                 }, status=200)
             else:
                 return JsonResponse({"error": "Error storing data"}, status=500)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
     return JsonResponse({"error": "Invalid request"}, status=400)
