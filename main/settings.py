b34db7816bac6b56f4bb55fbc9a3b00dbead1fce
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-22 09:50:05 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 
 CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/1')
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
