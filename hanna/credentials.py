d18d65f18c76bcdbf5e8d41cce2e0a1a4ebd99ea
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-27 18:51:44 +0530

    Update credentials.py

--- hanna/credentials.py
+++ hanna/credentials.py
@@ -14,8 +14,9 @@ class ClientCredentials:
     def __init__(self):
         self.cohere_client = cohere.Client(settings.COHERE_API_KEY)
         self.__auth_config = weaviate.auth.AuthApiKey(api_key=settings.WEAVIATE_API_KEY)
-        self.weaviate_client = weaviate.Client(
-            url=settings.WEAVIATE_URL,
-            additional_headers={"X-Cohere-Api-Key": settings.COHERE_API_KEY},
-            auth_client_secret=self.__auth_config
+        self.weaviate_client = weaviate.connect_to_local(
+            host=settings.WEAVIATE_HOST,
+            port=settings.WEAVIATE_PORT,
+            grpc_port=settings.WEAVIATE_GRPC_PORT,
+            additional_headers={"X-API-KEY": settings.WEAVIATE_API_KEY}
         )
