48ee7886ee136a7ddd4174b97a5e8901c7b8c543
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-27 18:56:49 +0530

    Update credentials.py

--- hanna/credentials.py
+++ hanna/credentials.py
@@ -12,11 +12,19 @@ load_dotenv()
 class ClientCredentials:
 
     def __init__(self):
-        self.cohere_client = cohere.Client(settings.COHERE_API_KEY)
-        self.__auth_config = weaviate.auth.AuthApiKey(api_key=settings.WEAVIATE_API_KEY)
-        self.weaviate_client = weaviate.connect_to_local(
-            host=settings.WEAVIATE_HOST,
-            port=settings.WEAVIATE_PORT,
-            grpc_port=settings.WEAVIATE_GRPC_PORT,
-            additional_headers={"X-API-KEY": settings.WEAVIATE_API_KEY}
-        )
+        try:
+            self.cohere_client = cohere.Client(settings.COHERE_API_KEY)
+            self.__auth_config = weaviate.auth.AuthApiKey(api_key=settings.WEAVIATE_API_KEY)
+            self.weaviate_client = weaviate.connect_to_local(
+                host="173.208.218.180",
+                port=8080,
+                grpc_port=50051,
+                additional_headers={"X-API-KEY": "hsdnfd7y3n87ry28gd989m82372t1e8hsey78t3291de"}
+            )
+            if self.weaviate_client.is_ready():
+                print("Weaviate connection established successfully.")
+            else:
+                print("Weaviate is not ready.")
+        except Exception as e:
+            print("Error connecting to Weaviate:", e)
+                
