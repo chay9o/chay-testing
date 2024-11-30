07c8fbe0fc00fab741bc8f2f65ab3dd09e0fa551
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-30 18:31:53 +0530

    Update urls.py

--- hanna/urls.py
+++ hanna/urls.py
@@ -4,14 +4,20 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 
-    # ---------- Vector Management ----------
-
+    # ---------- PPT GENERATOR ----------
     path('generate_ppt/', views.generate_ppt, name='generate_ppt'),
     path('get_task_status/<str:task_id>/', views.get_task_status, name='get_task_status'),
     path('get_task_status1/<str:task_id>/', views.get_task_status1, name='get_task_status1'),
     path('download_ppt/<str:task_id>/', views.download_ppt, name='download_ppt'),
     path('download_ppt1/<str:task_id>/', views.download_ppt1, name='download_ppt1'),
+    # ---------- ---------- ---------- ---------- ----------
+
+    # ---------- Strategic Alignment ----------
     path('evaluate_text', views.evaluate_text, name='evaluate_text'),
+    path('check_evaluation_status/<str:task_id>/', views.check_evaluation_status, name='check_evaluation_status'),
+    # ---------- ---------- ---------- ---------- ----------
+
+    # ---------- Depp Analysis ----------
     path('stinsight_step1', views.stinsight_step1, name='stinsight_step1'),
     path('stinsight_step2', views.stinsight_step2, name='stinsight_step2'),
     path('stinsight_step3', views.stinsight_step3, name='stinsight_step3'),
@@ -20,27 +26,37 @@ urlpatterns = [
     path('stinsight_step6', views.stinsight_step6, name='stinsight_step6'),
     path('DA_tester', views.DA_tester, name='DA_tester'),
     path('generate_questions', views.generate_questions, name='generate_questions'),
-  
+    path('get_ppt_for_option4/<str:task_id>/', views.get_ppt_for_option4, name='get_ppt_for_option4'),
+    # ---------- ---------- ---------- ---------- ----------
+
+    # ---------- Hanna Mind ----------
+    path('generate-prompt/', views.generate_prompt),
+    # ---------- ---------- ---------- ---------- ----------
+
+    # ---------- webhook for hanna anlytics ----------
     path('webhook_handler', views.webhook_handler, name='webhook_handler'),
-    path('view_classifications', views.view_classifications, name='view_classifications'),
-
-    path('create-solution/', views.create_solution, name='create_solution'),
-    path('delete-solutions/', views.delete_solutions, name='delete_solutions'),
+    # ---------- ---------- ---------- ---------- ----------
 
 
+    # ---------- Solution Hub ----------
+    path('create-solution/', views.create_solution),
+    path('delete-solution/', views.delete_solution),
+    path('update-solution/', views.update_solution),
+    # ---------- ---------- ---------- ---------- ----------
 
+    # ---------- Smart Notes ----------
+    path('create-collections/', views.create_collections),
     path('create-collection/', views.create_collection),
     path('add-object/text/', views.add_vectors_text),
     path('add-object/file/', views.add_vectors_file),
     path('add-object/all/', views.add_vectors_all),
     path('chat-summary/', views.chat_summary),
-    path('chat-view/', views.chat_view),
 
     path('update-object/text/', views.update_chunk_text),
     path('update-object/file/', views.update_chunk_file),
     path('update-object/all/', views.update_chunk_all),
 
-    path('remove-object/', views.remove_object),
+    # path('remove-object/', views.remove_object),
     path('remove-objects/entity/', views.remove_objects_entity),
     path('remove-objects/uuid/', views.remove_objects_uuid),
     path('remove-objects/uuids/', views.remove_objects_uuids),
@@ -49,32 +65,33 @@ urlpatterns = [
     path('remove-objects/all/', views.remove_objects_all),
     path('remove-objects/private-user/', views.remove_user_objects),
 
-    path('get-object/', views.get_object),
-    path('get-objects/entity/', views.get_objects_entity),
-    path('get-objects/uuid/', views.get_objects_uuid),
+    # path('get-object/', views.get_object),
+    # path('get-objects/entity/', views.get_objects_entity),
+    # path('get-objects/uuid/', views.get_objects_uuid),
     path('get-collection/', views.get_collection),
 
+    # TESTING SMARTNOTE SEARCH ALGO
     path('search-hybrid/', views.search_hybrid),
     # ---------- ---------- ---------- ---------- ----------
 
     # ---------- Master Vectors ----------
     path('create-master-collection/', views.create_master_collection),
-    path('add-master-object/text/', views.add_master_vectors),
+    # path('add-master-object/text/', views.add_master_vectors),
     path('add-master-object/file/', views.upload_master_file),
 
-    path('remove-master-object/', views.remove_master_object),
+    # path('remove-master-object/', views.remove_master_object),
     path('remove-master-objects/filename/', views.remove_master_objects_file),
     path('remove-master-objects/uuid/', views.remove_master_objects_uuid),
     path('remove-master-collection/', views.remove_master_collection),
 
-    path('get-master-object/', views.get_master_object),
-    path('get-master-objects/filename/', views.get_master_objects_filename),
-    path('get-master-objects/uuid/', views.get_master_objects_uuid),
-    path('get-master-objects/type/', views.get_master_objects_type),
+    # path('get-master-object/', views.get_master_object),
+    # path('get-master-objects/filename/', views.get_master_objects_filename),
+    # path('get-master-objects/uuid/', views.get_master_objects_uuid),
+    # path('get-master-objects/type/', views.get_master_objects_type),
     path('get-master-collection/', views.get_master_collection),
     # ---------- ---------- ---------- ---------- ----------
 
-    # ---- CALL THIS WHEN LIMIT EXCEEDS!!!!!
+    # ---- WARNING: CALL THIS WHEN IN DEV or TESTING!!!!!
     path('destroy/', views.destroy_all),
     # ---------- ---------- ---------- ---------- ----------
 
