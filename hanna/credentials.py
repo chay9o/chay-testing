6ce262a544782aaeaefb1fc3cb078c5c7484109c
chay9o <138090440+chay9o@users.noreply.github.com>
2024-11-30 18:32:22 +0530

    Update credentials.py

--- hanna/credentials.py
+++ hanna/credentials.py
@@ -1,5 +1,6 @@
 import cohere
 import weaviate
+from weaviate.classes.init import Auth
 from django.conf import settings
 from dotenv import load_dotenv
 import base64
@@ -9,13 +10,25 @@ warnings.simplefilter('ignore')
 
 load_dotenv()
 
+
 class ClientCredentials:
 
     def __init__(self):
         self.cohere_client = cohere.Client(settings.COHERE_API_KEY)
-        self.__auth_config = weaviate.auth.AuthApiKey(api_key=settings.WEAVIATE_API_KEY)
-        self.weaviate_client = weaviate.Client(
-            url=settings.WEAVIATE_URL,
-            additional_headers={"X-Cohere-Api-Key": settings.COHERE_API_KEY},
-            auth_client_secret=self.__auth_config
+        # self.__auth_config = weaviate.auth.AuthApiKey(api_key=settings.WEAVIATE_API_KEY)
+        # self.weaviate_client = weaviate.Client(
+        #     url=settings.WEAVIATE_URL,
+        #     additional_headers={"X-Cohere-Api-Key": settings.COHERE_API_KEY},
+        #     auth_client_secret=self.__auth_config
+        # )
+
+        self.__auth_config = Auth.api_key(settings.WEAVIATE_API_KEY)
+        self.weaviate_client = weaviate.connect_to_wcs(
+            skip_init_checks=True,
+            cluster_url=settings.WEAVIATE_URL,
+            auth_credentials=self.__auth_config,
+            headers={
+                "X-Cohere-Api-Key": settings.COHERE_API_KEY
+            }
         )
+
